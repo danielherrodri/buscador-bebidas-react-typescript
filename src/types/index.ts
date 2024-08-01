b22be506dd186d8ac3c5be10be38e3c6da373b57
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-08-01 02:56:22 -0600

    Se muestra información en cards de las bebidas consultadas

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
-import { CategoriesAPIResponseSchema, DrinksAPIResponse, SearchFilterScheme } from "../utils/recipe-schema";
+import { CategoriesAPIResponseSchema, DrinkAPIResponse, DrinksAPIResponse, SearchFilterScheme } from "../utils/recipe-schema";
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>
 export type SearchFilter = z.infer<typeof SearchFilterScheme>
 
 export type Drinks = z.infer<typeof DrinksAPIResponse>
+export type Drink = z.infer<typeof DrinkAPIResponse>
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,10 +1,24 @@
+import { useMemo } from "react";
 import { useAppStore } from "../stores/useAppStore";
+import DrinkCard from "../components/DrinkCard";
 export default function IndexPage() {
 
-  useAppStore((state) => state.categories)
+  const drinks = useAppStore((state) => state.drinks)
+
+  const hasDrinks = useMemo(() => drinks.drinks.length, [drinks])
+
   return (
     <>
-      <h1>Iniciuo</h1>
+      <h1 className="text-6xl font-extrabold">Recetas</h1>
+      {hasDrinks ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+          {drinks.drinks.map((drink) => (
+            <DrinkCard key={drink.idDrink} drink={drink} />
+          ))}
+        </div>
+      ) : (
+        <p className="my-10 text-center text-2xl">No hay mensajes aún, utiliza el formulario para buscar recetas.</p>
+      )}
     </>
   );
 }
