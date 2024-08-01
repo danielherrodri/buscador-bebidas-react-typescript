067d8b9d95824929cd794aa3f8ebe648ec7da441
drodriguez <drodriguez@izzi.mx>
2024-08-01 16:03:51 -0600

    Implementando múltiples stores en Zustand con slice pattern, e implementación de Free Cocktail API.

---
+++ src/stores/recipeSlice.ts
@@ -0,0 +1,20 @@
+import { StateCreator } from "zustand"
+import { getCategories } from "../services/RecipeService"
+import { Categories } from "../types"
+
+export type RecipeSliceType = {
+    categories: Categories,
+    fetchCategories: () => Promise<void>
+}
+
+export const createRecipeSlice: StateCreator<RecipeSliceType> = (set) => ({
+    categories: {
+        drinks: []
+    },
+    fetchCategories: async () => {
+        const categories = await getCategories()
+        set({
+            categories
+        })
+    }
+})
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,4 +1,7 @@
+import { useAppStore } from "../stores/useAppStore";
 export default function IndexPage() {
+
+  useAppStore((state) => state.categories)
   return (
     <>
       <h1>Inico</h1>
