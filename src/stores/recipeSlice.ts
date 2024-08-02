e213e2ee3dabc6165ba2f9f53a3b06ae44bce34d
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-08-02 00:12:07 -0600

    Colocando en el state la receta y su contenido

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,10 +1,11 @@
 import { StateCreator } from "zustand"
 import { getCategories, getRecipeById, getRecipies } from "../services/RecipeService"
-import { Categories, Drink, Drinks, SearchFilter } from "../types"
+import { Categories, Drink, Drinks, Recipe, SearchFilter } from "../types"
 
 export type RecipeSliceType = {
     categories: Categories,
     drinks: Drinks,
+    selectedRecipe: Recipe,
     fetchCategories: () => Promise<void>,
     searchRecipes: (searchFilter: SearchFilter) => Promise<void>,
     selectRecipe: (id: Drink['idDrink']) => Promise<void>
@@ -17,6 +18,7 @@ export const createRecipeSlice: StateCreator<RecipeSliceType> = (set) => ({
     drinks: {
         drinks: []
     },
+    selectedRecipe: {} as Recipe,
     fetchCategories: async () => {
         const categories = await getCategories()
         set({
@@ -30,7 +32,9 @@ export const createRecipeSlice: StateCreator<RecipeSliceType> = (set) => ({
         })
     },
     selectRecipe: async (id) => {
-        const drink = await getRecipeById(id)
-        console.log(drink)
+        const selectedRecipe = await getRecipeById(id)
+        set({
+            selectedRecipe
+        })
     }
 })
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,9 @@
 import { z } from "zod";
-import { CategoriesAPIResponseSchema, DrinkAPIResponse, DrinksAPIResponse, SearchFilterScheme } from "../utils/recipe-schema";
+import { CategoriesAPIResponseSchema, DrinkAPIResponse, DrinksAPIResponse, RecipeAPIResponseSchema, SearchFilterScheme } from "../utils/recipe-schema";
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>
 export type SearchFilter = z.infer<typeof SearchFilterScheme>
 
 export type Drinks = z.infer<typeof DrinksAPIResponse>
 export type Drink = z.infer<typeof DrinkAPIResponse>
+export type Recipe = z.infer<typeof RecipeAPIResponseSchema>
