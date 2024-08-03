fd800df9d49c8b4c7c70a1214595920c17a9453e
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-08-03 00:38:39 -0600

    Agregando bebida a favoritos

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,6 +7,7 @@ export default function Modal() {
     const modal = useAppStore((state) => state.modal)
     const closeModal = useAppStore((state) => state.closeModal)
     const selectedRecipe = useAppStore((state) => state.selectedRecipe)
+    const handleClickFavorite = useAppStore((state) => state.handleClickFavorite)
 
     const renderIngredients = () => {
         const ingredients: JSX.Element[] = []
@@ -71,7 +72,7 @@ export default function Modal() {
                                             <button type='button' className='w-full rounded bg-gray-600 p-3 font-bold uppercase text-white shadow hover:bg-gray-500' onClick={closeModal}>Cerrar</button>
                                         </div>
                                         <div className='mt-5 flex justify-between gap-4'>
-                                            <button type='button' className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500'>Agregar a Favoritos</button>
+                                            <button type='button' className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500' onClick={() => handleClickFavorite(selectedRecipe)}>Agregar a Favoritos</button>
                                         </div>
                                     </Dialog.Title>
                                 </Dialog.Panel>
--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,12 +1,14 @@
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { createRecipeSlice, RecipeSliceType } from './recipeSlice'
+import { createFavoritesSlice, FavoritesSliceType } from './favoritesSlice'
 
-export const useAppStore = create<RecipeSliceType>()(
+export const useAppStore = create<RecipeSliceType & FavoritesSliceType>()(
     devtools(
         (...a) => (
             {
-                ...createRecipeSlice(...a)
+                ...createRecipeSlice(...a),
+                ...createFavoritesSlice(...a)
             }
         )
     )
