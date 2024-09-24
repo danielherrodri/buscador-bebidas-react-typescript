c4ec1b00093fbdadb872e59ee5dc2a9c7c4ccf7b
drodriguez <drodriguez@izzi.mx>
2024-09-24 11:55:34 -0600

    Implementación de sistema de notificaciones

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -2,13 +2,15 @@ import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { createRecipeSlice, RecipeSliceType } from './recipeSlice'
 import { createFavoritesSlice, FavoritesSliceType } from './favoritesSlice'
+import { NotificationSliceType, createNotificationSlice } from './notificationSlice'
 
-export const useAppStore = create<RecipeSliceType & FavoritesSliceType>()(
+export const useAppStore = create<RecipeSliceType & FavoritesSliceType & NotificationSliceType>()(
     devtools(
         (...a) => (
             {
                 ...createRecipeSlice(...a),
-                ...createFavoritesSlice(...a)
+                ...createFavoritesSlice(...a),
+                ...createNotificationSlice(...a)
             }
         )
     )
